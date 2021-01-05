be00b6d4fcf00ace6d16b22619b117597b674a57
ubuntu <ubuntu@restate.com>
2021-01-04 23:48:01 -0800

    from ame

--- app.py
+++ app.py
@@ -37,42 +37,6 @@ key = '90dc4b25c570c60b83ebefe5d98aedcc'
 g_code = 'RSFST03001' # 寿司のコード
 address = '那覇'
 
-# API を使う関数の定義
-def gnavi_api(g_code,address):
-    params = urllib.parse.urlencode({
-        'keyid': key,
-        # 'name' : shop_name,　#店名も含める場合はコメントアウト外す
-        'category_s' : g_code,
-        'address' : address
-    })
-    url = base_url + '?' + params
-    print(url)
-    response = urllib.request.urlopen(url,context=context)
-    return response.read()
-
-
-# お店ごとにループし、お店の名前を list に追加する関数の定義
-def get_name(read_data):
-    for dic in read_data:
-        list_name.append(dic.get("name"))
-        #list_name.append(dic.get("address")) #住所を取得したい場合はこちら
-    #return list_name
-
-
-def base_api():
-    # 関数を使って、API から情報を取得
-    data = gnavi_api(g_code,address)
-
-    # 取得した情報をJSON形式から辞書型に変換
-    read_data = json.loads(data)["rest"]
-
-    # お店の名前の一覧を格納する list の作成
-    list_name = []
-
-
-    # 関数を実行し、お店の名前の list を取得
-    get_name(read_data)
-    return(list_name)
 #*******************   gurunavi             ***************
 
 
@@ -137,7 +101,7 @@ def callback():
                 'address' : address
             })
             url = base_url + '?' + params
-            print(url)
+            #print(url)
             response = urllib.request.urlopen(url,context=context)
             data = response.read()
 
